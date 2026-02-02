3b577fa4d0f3e960b4f940ac31d0d5d8097765a2
abxcyzz-art <abxcyzz@gmail.com>
2026-02-02 11:10:23 +0700

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,2 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+Wizard wizardA = new Wizard("Harre Potta", 20);
+Wizard wizardB = new Wizard(" Youre a Wizard Harry", 30);
+
+Console.WriteLine("Permainan Dimulai...\n");
+wizardA.ShowStat();
+wizardB.ShowStat();
+
+wizardA.Attack(wizardB);
+wizardB.Attack(wizardA); 
+wizardA.Attack(wizardB);
+
+
+public class Wizard
+{
+    public string Name;
+    public int Energi;
+    public int Damage;
+
+    public Wizard(string name, int damage)
+    {
+        Name = name;
+        Energi = 100;
+        Damage = damage;
+    }
+
+    public void ShowStat()
+    {
+        Console.WriteLine("Statistik Wizard");
+        Console.WriteLine($"Nama: {Name}, Energi: {Energi} \n");
+    }
+
+    public void Attack(Wizard wizardLawanObj)
+    {
+        wizardLawanObj.Energi -= Damage;
+        Console.WriteLine($"{Name} menyerang {wizardLawanObj.Name}");
+        Console.WriteLine($"Sisa energi {wizardLawanObj.Name} adalah {wizardLawanObj.Energi}");
+    }
+
+}
